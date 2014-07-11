83de0403f80f5f8ba84537df34d77fcf2dee9c38
PJ Fancher <pj@digitalbrands.com>
2014-07-11 14:16:41 +0000

    Removed if statement

--- add-sites-available.php
+++ add-sites-available.php
@@ -3,9 +3,6 @@
 	 * NEEDS TO BE RUN AS SUDO
     *****************************************************************************/
 
-	//PATH TO VIRTUAL HOST FILES
-	$path = '/etc/apache2/sites-available/';
-
 	//SERVER ADMIN
 	$server_admin = 'pj@digitalbrands.com';
 
@@ -25,16 +22,13 @@
 		'ha',   //HOSTINGADVICE
 	);
 
+	//PATH TO VIRTUAL HOST FILES
+	$path = '/etc/apache2/sites-available/';
 
 	//LOOP THORUGH EACH SITE AND CREATE THE VIRTUAL HOST FILE
 	$stdout = fopen('php://stdout', 'w');
 	foreach( $site_prefixes as $site_prefix ) :
-		if( empty( $site_prefix ) ) :
-			$site_url = $site_domain;
-		else :
-			$site_url = $site_prefix . '.' . $site_domain;
-		endif;
-		
+		$site_url = empty( $site_prefix ) ? $site_domain : $site_prefix . '.' . $site_domain;
 		$file_handle = fopen( $path . $site_url . '.conf', 'w' );
 		fwrite( $stdout, "Writing file: $site_url\n" );
 		fwrite( $file_handle,
